9406f6b17500e890ea8a1b5a92d9932b3c63d5a5
pjasani <49772210+pjasani@users.noreply.github.com>
2020-03-11 01:02:19 -0500

    NewRender+NewBuilding+Windzone + CameraFix
    Baked the new environment after removing extra objects .
    Added building on the left of the school.
    Added Windzone.
    Fixed the post processing (has not been activated yet)
    Added Anti-aliasing
    Added the Main Scene

---
+++ Assets/MyProduct/Scripts/CamGyro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamGyro : MonoBehaviour
+{
+    GameObject camParent;
+    public Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camParent = new GameObject("CamParent");
+        camParent.transform.position = this.transform.position;
+        this.transform.parent = camParent.transform;
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camParent.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);     //Xroation from the gyroscope
+        this.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0); //Yroation for from the gyroscope
+        //Debug.Log(-Input.gyro.rotationRateUnbiased.x);
+        //playerBody.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+    }
+}
